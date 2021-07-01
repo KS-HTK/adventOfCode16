5da9ab84b45f2bea3e2ddce515f2a5eedeb83fec
KS_HTK <HKoertge@gmx.de>
2021-07-01 02:11:28 +0200

    solved day11

--- day11/day11.js
+++ day11/day11.js
@@ -1,7 +1,18 @@
-const fs = require('fs');
-
-let rawdata = fs.readFileSync('./day11/input');
-let setup = rawdata.toString().split("\n");
-
-console.log("Part 1: ", setup);
-console.log("Part 2:");
+// this code is slightly problematic as it ignores difficult situation (fring microchips)
+// it does not work for all inputs
+let floors = [["sg", "pg", "sm", "pm"], ["tg", "rg", "cg", "rm", "cm"], ["tm"], []];
+console.log("Part 1:", countSteps(floors));
+floors = [["sg", "pg", "sm", "pm", "eg", "em", "dg", "dm"], ["tg", "rg", "cg", "rm", "cm"], ["tm"], []];
+console.log("Part 2:", countSteps(floors));
+function countSteps(floors) {
+                   let e = 0;
+                   let steps = 0;
+                   let elevate = []
+                   floors.forEach((floor, i) => {
+                       if (i == floors.length-1) return;
+                       floor.push(...elevate);
+                       steps += ((floor.length*2)-3);
+                       elevate = floor;
+                   });
+                   return steps;
+}
