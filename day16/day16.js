d0dc193e0758c1aa0402640891891ef95c0fe0b8
KS_HTK <HKoertge@gmx.de>
2021-07-31 05:26:02 +0200

    rewritten code

--- day16/day16.js
+++ day16/day16.js
@@ -1,10 +1,21 @@
 const input = '01110110101001000';
 const d1size = 272;
 const d2size = 35651584;
+//b = a reversed inverted
+//a0b 0 a1b  0  a0b 1 a1b   0   a0b 0 a1b  1  a0b 1 a1b
+//sep = 0010011000110110001001110011011
 let invIn ='';
 [...input].forEach(c => invIn = (c=='0'?'1':'0')+invIn);
-//a0b 0 a1b  0  a0b 1 a1b   0   a0b 0 a1b  1  a0b 1 a1b
-//0010011000110110001001110011011
+//checksum for a+'0' and a+'1'
+let aSums = {
+    '0': checksum([...input, '0']),
+    '1': checksum([...input, '1'])
+}
+//checksum for b+'0' and b+'1'
+let bSums = {
+    '0': checksum([...invIn, '0']),
+    '1': checksum([...invIn, '1'])
+}
 let len = 17;
 let sep = '0';
 while (len < d2size) {
@@ -14,31 +25,35 @@ while (len < d2size) {
     [...sep].forEach(c => tmp = (c=='0'?'1':'0')+tmp);
     sep = sep+'0'+tmp;
 }
+console.log("Part 1:", ''.concat(...checksum(firstCsum(d1size))));
+let fSum = firstCsum(d2size);
+console.log(fSum);
+console.log("Part 2:", ''.concat(...checksum(fSum)));
 
-console.log("Part 1:", checksum(getData(input, sep, invIn, d1size)));
-console.log("Part 2:", checksum(getData(input, sep, invIn, d2size)));
-
-function getData(input, sep, invIn, len) {
-    let dragon = '';
+function firstCsum(len) {
+    let csum = [];
     let ind = 0;
-    while (dragon.length < len) {
-        dragon += input+sep[ind++]+invIn+sep[ind++];
+    while(len > 36) {
+        let s1 = sep[ind++];
+        let s2 = sep[ind++];
+        csum = csum.concat(aSums[s1],bSums[s2]);
+        len-=36;
     }
-    dragon = dragon.slice(0, len);
-    let data = [];
-    [...dragon].forEach(c => data.push(c=='1'));
-    return data;
+    if (len >= 18) {
+        len-=18;
+        csum = csum.concat(aSums[sep[ind]], checksum([...invIn.slice(0, len)]));
+    } else {
+        csum = csum.concat(checksum([...input.slice(0, len)]));
+    }
+    return csum;
 }
 
 function checksum(data) {
+    if (!Array.isArray(data)) throw new TypeError("data is of wrong type.");
+    if (data.length%2==1) return data;
     let csum = [];
     while (data.length) {
-        csum.push(data.shift() == data.shift());
+        csum.push((data.shift() == data.shift())?'1':'0');
     }
-    if (csum.length%2==0) return checksum(csum);
-    else {
-        let res = '';
-        csum.forEach(b => {res += b? '1':'0';});
-        return res;
-    };
+    return checksum(csum);
 }
